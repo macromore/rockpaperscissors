0523734b637bd4814f256f8b919f50a698aaf188
David Blessing <8893118+macromore@users.noreply.github.com>
2025-07-18 12:05:29 -0400

    Add function playRound

--- javascript.js
+++ javascript.js
@@ -12,8 +12,56 @@ function getComputerChoice() {
 }
 
 function getHumanChoice () {
-    return prompt("Choose: rock, paper, or scissors:");
+    let x = prompt("Choose: rock, paper, or scissors:");
+    console.log("You chose "+x+".");
+    return x;
+}
+
+
+function playRound(humanSelection, computerSelection) {
+    console.log("... computer chooses "+computerSelection+".");
+    switch (humanSelection.toLowerCase()) {
+        case "rock":
+            if (computerSelection == "rock") {
+                console.log("It's a TIE!");
+            } else if (computerSelection == "paper") {
+                console.log("Computer wins! You LOSE!");
+                computerScore++;
+            } else {
+                console.log("You win! How could you?");
+                humanScore++;
+            }
+            break;
+        case "paper":
+            if (computerSelection == "rock") {
+                console.log("You win! How could you?");
+                humanScore++;
+            } else if (computerSelection == "paper") {
+                console.log("It's a TIE!");
+            } else {
+                console.log("Computer wins! You LOSE!");
+                computerScore++;
+            }
+            break;
+        case "scissors":
+            if (computerSelection == "rock") {
+                console.log("Computer wins! You LOSE!");
+                computerScore++;
+            } else if (computerSelection == "paper") {
+                console.log("It's a TIE!");
+            } else {
+                console.log("You win! How could you?");
+                humanScore++;
+            }
+            break;
+    }
 }
 
 let humanScore = 0;
 let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+playRound(humanSelection, computerSelection);
