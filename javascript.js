f306eff5f0dab39fabe17e662c396bac34a62e21
David Blessing <8893118+macromore@users.noreply.github.com>
2025-07-18 11:41:35 -0400

    Add getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
 console.log("Hello world!");
+
+// Get computer choice
+
+function getComputerChoice() {
+    let x = Math.random();
+    if (x <= 0.333) {
+        return "rock";
+    } else if (x >= 0.666) {
+        return "paper"; 
+    } else {
+        return "scissors";
+    }
+}
